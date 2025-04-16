99f27c7667e7970b928ba76d7c64d8602a57a0f0
jacobkim9881 <jacobkim9881@gmail.com>
2025-04-16 12:41:07 +0900

    dlog can be used for cc.log or console.log

--- assets/Script/Logger.js
+++ assets/Script/Logger.js
@@ -1,13 +1,13 @@
 const isDevMode = cc.sys.isBrowser && cc.debug;
 
 window.dlog = (...args) => {
-    if (isDevMode) {
-        cc.log(...args); // ✅ `cc.log()`가 자기 자신을 호출하지 않도록 수정
-    }
+  if (isDevMode) {
+    cc.log(...args); // ✅ `cc.log()`가 자기 자신을 호출하지 않도록 수정
+  }
 };
 
 window.dbg = (...args) => {
-    if (isDevMode) {
-        console.log(...args); // ✅ `dbg()`가 자기 자신을 호출하지 않도록 수정
-    }
+  if (isDevMode) {
+    console.log(...args); // ✅ `dbg()`가 자기 자신을 호출하지 않도록 수정
+  }
 };
